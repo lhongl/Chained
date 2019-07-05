db7a51e7c8869a20d4d2e6e51f28f2d110bb753a
李宏亮 <lihongliang@freedotech.com>
2019-07-05 14:21:49 +0800

    头文件添加

---
+++ Chained/HLChainedHeader.h
@@ -0,0 +1,19 @@
+//
+//  HLChainedHeader.h
+//  Chained
+//
+//  Created by 飞渡 on 2019/7/5.
+//  Copyright © 2019 freedo. All rights reserved.
+//
+
+#ifndef HLChainedHeader_h
+#define HLChainedHeader_h
+
+#import "UILabel+Chained.h"
+#import "UITextField+Chained.h"
+#import "UIButton+Chained.h"
+#import "UIView+Chained.h"
+#import "UITextView+Chained.h"
+
+
+#endif /* HLChainedHeader_h */
